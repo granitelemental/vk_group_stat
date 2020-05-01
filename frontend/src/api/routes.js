f22c8e9f518025a661b61ddb5ea1043e42eb88c1
Trdat Mkrtchian <zeffirsky@yandex-team.ru>
2020-05-01 21:58:38 +0300

    Fix

--- frontend/src/api/routes.js
+++ frontend/src/api/routes.js
@@ -2,6 +2,12 @@ import api from './index';
 
 export const getTopPosts = (duration) => {    
     return api.get(`/stats/posts/top?duration=${duration}`).then(({data}) => {
-        return data
+        return {
+            id: data.id,
+            comments: data.comments_count,
+            reposts: data.reposts_count,
+            likes: data.likes_count,
+            view: data.view_count,
+        }
     });
 }
